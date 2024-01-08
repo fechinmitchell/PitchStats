0cacfb9275127583ecbc7a331086d9ce035542f8
Fechinmitchell <fechinmitchell1996@gmail.com>
2024-01-08 20:27:49 +1100

    Initial Commit

---
+++ PitchStatsV2/PitchStatsV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PitchStatsV2App.swift
+//  PitchStatsV2
+//
+//  Created by Fechin Mitchell on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PitchStatsV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PitchStatsV2UITests/PitchStatsV2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PitchStatsV2UITestsLaunchTests.swift
+//  PitchStatsV2UITests
+//
+//  Created by Fechin Mitchell on 08/01/2024.
+//
+
+import XCTest
+
+final class PitchStatsV2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
